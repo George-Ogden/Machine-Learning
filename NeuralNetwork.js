44506c9eef6cc085ff5f323bd8aca6cfdeebd6fd
George Ogden <G02287@outlook.com>
2019-12-26 16:34:53 +0000

    Neural Network constructor

--- NeuralNetwork.js
+++ NeuralNetwork.js
@@ -1,5 +1,19 @@
 class Neural_Network{
     constructor(inputs,hidden_layers,layer_thickness, outputs,activation_function){
-        
+        //initialise variables
+        this.length = hidden_layers + 2;
+        this.width = layer_thickness;
+        //creates weights and biases matrices
+        this.weights =  [];
+        for (let i = 0; i < this.length; i++){
+            sample.push(new Matrix(this.width,this.width));
+        }
+        this.weights[0] = new Matrix(inputs,this.width);
+        this.weights[this.length-1] = new Matrix(this.width,outputs);
+        this.biases = [];
+        for (let i = 0; i < this.length; i++){
+            sample.push(new Matrix(1,this.width));
+        }
+        this.biases[this.length-1] = new Matrix(1,outputs);
     }
 }
